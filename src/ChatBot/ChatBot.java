804af80f972b071214861e4f2fbe9de0756c57bd
avvileiskyi <a.v.vileiskyi@student.khai.edu>
2024-12-18 00:55:18 +0200

    ChatBot 3-rd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -5,8 +5,14 @@ public class ChatBot {
         System.out.println("Hello! My name is ChatBot.");
         System.out.println("I was created in 2024.");
 
-        
+
+        String name = "User";
         System.out.println("Please, remind me your name.");
-        System.out.println("What a great name you have, User!");
+        System.out.println("What a great name you have, " + name + "!");
+
+        
+        int remainder3 = 1, remainder5 = 2, remainder7 = 3;
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
 }
