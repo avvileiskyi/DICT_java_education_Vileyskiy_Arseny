dcd4652742070b695f57b05c53c5c999557b36fa
avvileiskyi <a.v.vileiskyi@student.khai.edu>
2024-12-18 00:56:19 +0200

    ChatBot 4-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -10,9 +10,18 @@ public class ChatBot {
         System.out.println("Please, remind me your name.");
         System.out.println("What a great name you have, " + name + "!");
 
-        
+
         int remainder3 = 1, remainder5 = 2, remainder7 = 3;
         int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+
+
+        System.out.println("Now I will prove to you that I can count to any number you want!");
+
+        
+        int userInp = 5;
+        for (int i = 1; i <= userInp; i++) {
+            System.out.println(i + "!");
+        }
     }
 }
