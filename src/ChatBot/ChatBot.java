a1d115f34a62b666508b21ee3ee6ad06484f40b5
avvileiskyi <a.v.vileiskyi@student.khai.edu>
2024-12-18 00:54:10 +0200

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,4 +1,12 @@
 package ChatBot;
 
 public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is ChatBot.");
+        System.out.println("I was created in 2024.");
+
+        
+        System.out.println("Please, remind me your name.");
+        System.out.println("What a great name you have, User!");
+    }
 }
